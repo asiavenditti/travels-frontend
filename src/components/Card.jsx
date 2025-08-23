27c74adbdd33930db5d461ac09e751c29b203bdd
Your Name <you@example.com>
2025-08-23 11:35:19 +0200

    Refactor Card component to handle image loading errors and update HomePage to fetch and display city data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,30 @@
 
+import { useState } from "react"
 
+export default function Card({ altText, imageUrl, title, description }) {
 
-export default function Card() {
+    const [imageErr, setImageErr] = useState(false)
+
     return (
-        <div className="card" style={{ width: 18rem }}>
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+        <div>
+            <div className="card" style={{ width: '18rem' }}>
+                {imageErr ? (
+                    <div className="d-flex justify-content-center align-items-center bg-secondary text-white" style={{ height: '18rem' }}>
+                        <p className="m-0">Immagine non disponibile</p>
+                    </div>
+                ) : (
+
+                    <img
+                        src={imageUrl}
+                        alt={altText}
+                        className="card-img-top"
+                        onError={setImageErr(true)}
+                    />
+                )}
+                <div className="card-body">
+                    <h5 className="card-text">{title}</h5>
+                    <p className="card-text">{description}</p>
+                </div>
             </div>
         </div>
     )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,37 @@
-
+import { use, useEffect, useState } from "react";
+import Card from "../components/Card";
 export default function HomePage() {
 
+    const [cities, setCities] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3000/cities')
+            .then(res => res.json())
+            .then(data => setCities(data))
+            .catch(err => console.error('Fetch error'))
+    }, [])
+
+    return (
+        <div className="container">
+            <h2 className="text-center my-5">Le nostre destinazioni</h2>
+            <div className="row ">
+                {cities.map((city) => {
+                    return (
+                        <div className="col-sm-12 col-md-4 d-flex justify-content-center my-2" key={city.id}>
+
+
+                            <Card
+                                imageUrl={city.imageUrl}
+                                altText={city.city}
+                                title={city.city}
+                                description={city.country}
+                            />
+                        </div>
+
+                    )
+                })}
+            </div>
+        </div>
+    )
 
 }
